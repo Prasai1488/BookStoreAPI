d5dfd0f4fcf893a602d17fd4f852926f33edb477
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-05 22:01:13 +0545

    created default admin account

---
+++ BookStore.API/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookStore.API.Models;
+using BookStore.API.Enums;
+
+namespace BookStore.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Store enum as string for readability
+            modelBuilder.Entity<User>()
+                .Property(u => u.Role)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Book>()
+    .Property(b => b.Genre)
+    .HasConversion<string>();
+
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Format)
+                .HasConversion<string>();
+
+        }
+    }
+}
---
+++ BookStore.API/Data/Seed/DbSeeder.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.API.Enums;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookStore.API.Data.Seed
+{
+    public static class DbSeeder
+    {
+        public static async Task SeedAdminAsync(AppDbContext context)
+        {
+            // Apply pending migrations automatically
+            await context.Database.MigrateAsync();
+
+            // Check if an admin already exists
+            if (!await context.Users.AnyAsync(u => u.Role == UserRole.Admin))
+            {
+                var adminUser = new User
+                {
+                    Name = "Super Admin",
+                    Email = "prasaiprithvi4@gmail.com",
+                    Password = BCrypt.Net.BCrypt.HashPassword("Admin@123"),  // Hash the password
+                    Role = UserRole.Admin,
+                    MembershipId = null
+                };
+
+                context.Users.Add(adminUser);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
