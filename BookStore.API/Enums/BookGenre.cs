6e07636c92125dea55d336ef347f540eef90641b
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-05 21:57:50 +0545

    Created enums for book and user related model

---
+++ BookStore.API/Enums/BookGenre.cs
@@ -0,0 +1,28 @@
+﻿namespace BookStore.API.Enums
+{
+    public enum BookGenre
+    {
+        Fiction,
+        NonFiction,
+        Biography,
+        History,
+        Science,
+        Mystery,
+        Fantasy,
+        Romance,
+        SelfHelp,
+        Other
+    }
+
+    public enum BookFormat
+    {
+        Hardcover,
+        Paperback,
+        CollectorEdition,
+        SignedEdition,
+        DeluxeEdition,
+        Ebook,
+        Other
+    }
+
+}
---
+++ BookStore.API/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.API.Enums
+{
+    public enum UserRole
+    {
+        Member = 0,
+        Staff = 1,
+        Admin = 2
+    }
+}
