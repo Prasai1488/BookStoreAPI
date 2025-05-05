f6337f97eebf0b734b9c119b253c04cdab081762
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-05 22:17:44 +0545

    Created book related DTOs

---
+++ BookStore.API/DTOs/Books/BookResponseDto.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.API.Enums;
+
+namespace BookStore.API.DTOs.Books
+{
+    public class BookResponseDto
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public BookGenre Genre { get; set; }
+        public string Language { get; set; }
+        public BookFormat Format { get; set; }
+        public string Publisher { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsExclusive { get; set; }
+        public bool OnSale { get; set; }
+        public decimal? SalePrice { get; set; }
+        public DateTime? SaleStart { get; set; }
+        public DateTime? SaleEnd { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
+
---
+++ BookStore.API/DTOs/Books/CreateBookDto.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.DTOs.Books
+{
+    public class CreateBookDto
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public BookGenre Genre { get; set; }
+
+        public string Language { get; set; }
+
+        [Required]
+        public BookFormat Format { get; set; }
+
+        public string Publisher { get; set; }
+
+        public DateTime? PublicationDate { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public bool IsExclusive { get; set; }
+
+        public bool OnSale { get; set; }
+
+        public decimal? SalePrice { get; set; }
+
+        public DateTime? SaleStart { get; set; }
+
+        public DateTime? SaleEnd { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
