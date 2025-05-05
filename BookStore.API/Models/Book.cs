899117793d5e863f2f3f9bc96832e92fb96e0838
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-05 21:58:20 +0545

    created user and book model

---
+++ BookStore.API/Models/Book.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BookStore.API.Enums;
+
+namespace BookStore.API.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }  // Primary Key
+
+        [Required]
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public BookGenre Genre { get; set; } 
+
+        public string Language { get; set; }
+
+        public BookFormat Format { get; set; } 
+
+        public string Publisher { get; set; }
+
+        public DateTime? PublicationDate { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public bool IsExclusive { get; set; }  // e.g., Signed, Deluxe, Collector’s
+
+        public bool OnSale { get; set; }
+
+        public decimal? SalePrice { get; set; }
+
+        public DateTime? SaleStart { get; set; }
+
+        public DateTime? SaleEnd { get; set; }
+    }
+}
---
+++ BookStore.API/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }  // Primary Key
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }  // Should be hashed
+
+        [Required]
+        public UserRole Role { get; set; } = UserRole.Member;
+
+        public string? MembershipId { get; set; }
+    }
+}
