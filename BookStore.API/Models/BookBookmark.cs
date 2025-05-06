d3ee2ed95cca3c728dd579200c5ea6cac5f92efa
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-06 19:59:35 +0545

    Created Book Bookmark model

---
+++ BookStore.API/Models/BookBookmark.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.API.Models
+{
+    public class BookBookmark
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        // Navigation (optional)
+        public Book Book { get; set; }
+        public User User { get; set; }
+    }
+}
