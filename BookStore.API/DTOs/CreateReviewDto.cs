2113a91313d10cd3a00638cb4c4dd08bf0f3ce75
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-06 20:56:09 +0545

    created review DTOs

---
+++ BookStore.API/DTOs/CreateReviewDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.DTOs
+{
+    public class CreateReviewDto
+    {
+        public int BookId { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+    }
+
+}
