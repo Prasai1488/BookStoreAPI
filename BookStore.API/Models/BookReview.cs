0a77d159eb2cf34ed68dd0ea8645974922205019
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-06 19:59:47 +0545

    Created Book reviews model

---
+++ BookStore.API/Models/BookReview.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Models
+{
+    public class BookReview
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int BookId { get; set; }
+
+        public int UserId { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [MaxLength(1000)]
+        public string Comment { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Book Book { get; set; }
+        public User User { get; set; }
+    }
+
+}
