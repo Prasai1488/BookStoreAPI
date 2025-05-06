e2d4faf1661b4f4ab724f9aa9b5583c2d2ad5acc
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-06 20:00:07 +0545

    Created Orders related model

---
+++ BookStore.API/Models/Order.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public int UserId { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string ClaimCode { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        public List<OrderItem> Items { get; set; }
+    }
+
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int BookId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal PriceAtPurchase { get; set; }
+
+        public Book Book { get; set; }
+    }
+
+}
