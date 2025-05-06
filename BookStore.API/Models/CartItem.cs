fd4fc65485aec186a0fb7404e09d12e355a607c7
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-06 19:59:57 +0545

    Created Cart item model

---
+++ BookStore.API/Models/CartItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        public int Quantity { get; set; } = 1;
+
+        // Optional navigation
+        public User User { get; set; }
+        public Book Book { get; set; }
+    }
+}
