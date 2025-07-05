218185fa8168a5e95482c7d2c422c9dc3b9e9f19
Prasai1488 <prasaiprithvi4@gmail.com>
2025-07-05 18:22:16 +0545

    Added deployed Vercel domain to CORS

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://book-store-ui-rust.vercel.app/")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // Needed for cookies/auth headers
